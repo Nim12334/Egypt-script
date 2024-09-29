2688a431d2195ab2b143cbe368e9273de7c5c78b
Nim12334 <ninlin6859@gmail.com>
2024-09-29 21:33:44 +0300

    Add files via upload
    version 2.0

---
+++ egypt.py
@@ -0,0 +1,31 @@
+import numpy as np 
+import scipy
+def write(text,steps):
+    while(b):
+     print(a)
+class math:
+   def sin(number):
+      return np.sin(number)
+   def cos(number):
+      return np.cos(number)
+   def tan(number):
+      return np.tan(number)
+   def exp(number):
+      return np.exp(number)
+   def log(number):
+      return np.log(number)
+   def minim(number):
+      return np.min(number)
+   def maxim(number):
+      return np.max(number)
+   def root(number):
+      return np.sqrt(number)
+   def cubert(number):
+      return np.cbrt(number)
+   def e():
+      return np.e
+   def pi():
+      return np.pi
+   def i0(number):
+      return np.i0(number)
+      
